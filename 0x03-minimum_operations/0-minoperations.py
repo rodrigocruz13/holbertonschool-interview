7acfb9bac760525d9f297a40ba67c4d297abae76
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-02-23 23:52:26 -0500

    Min ops

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -15,14 +15,14 @@ def minOperations(n):
         0 if it is impossible to achieve
     """
 
-    # if (not isinstance(n, int)):
-    #     return 0
+    if (not isinstance(n, int)):
+        return 0
 
     if (n < 2):
         return 0
 
     res = 0
-    for i in range(2, n):
+    for i in range(2, n + 1):
         while n % i == 0:
             res += i
             n = n / i
