58516489a28da5610720d93b782aafb48bcf930b
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-02-23 23:37:18 -0500

    Min ops

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module used to add two arrays
+"""
+
+
+def minOperations(n):
+    """
+    Method that determines if all the boxes can be opened.
+    Args:
+        n (int): Number.
+
+    Returns:
+        x (int): for success,
+        0 if it is impossible to achieve
+    """
+
+    if (not isinstance(n, int)):
+        return 0
+
+    if (n < 0):
+        return 0
+    
+    res = 0;
+    
+    for i in range (2, n):
+        while n % i == 0:
+            res += i
+            n = n/i
+    return res
