078b4174736475b822a755ba67e6f471ce51020a
rodrigocruz13 <rodrigocruz13>
2020-03-09 13:03:30 -0500

    Fixed functions

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -10,36 +10,12 @@
 int is_palindrome(listint_t **head)
 {
 	const listint_t *current;
-	unsigned int n; /* number of nodes */
-	int palind = 0;
+	int i, tam;
+	int array_right[32000];
+	int array_inverse[32000];
 
 	current = *head;
-
-	/* Calulate size */
-	if (current != NULL)
-		n = size_listint(current);
-	if (n > 0)
-	{
-		palind = calculate_palind(current, n);
-		return (palind);
-	}
-	return (0);
-}
-
-/**
- * calculate_palind- prints all elements of a listint_t list
- * @h: pointer to head of list
- * @tam: size of the linked list
- * Return: 1 if palindrome, 0 if not
- */
-int calculate_palind(const listint_t *h, int tam)
-{
-	int i;
-	int *array_right = malloc(sizeof(int) * tam);
-	int *array_inverse = malloc(sizeof(int) * tam);
-	const listint_t *current = h;
-
-	for (i = 0; current != NULL; current = current->next, i++)
+	for (i = 0, tam = 0; current != NULL; current = current->next, i++, tam++)
 	{
 		array_right[i] = current->n;
 		array_inverse[tam - i - 1] = current->n;
@@ -51,21 +27,4 @@ int calculate_palind(const listint_t *h, int tam)
 			return (0);
 	}
 	return (1);
-}
-
-/**
- * size_listint- calculates the size of a listint_t list
- * @h: pointer to head of list
- * Return: The size of a list
- */
-size_t size_listint(const listint_t *h)
-{
-	const listint_t *current;
-	unsigned int n; /* number of nodes */
-
-	current = h;
-	for (n = 0; current != NULL; current = current->next, n++)
-	{
-	}
-	return (n);
 }
