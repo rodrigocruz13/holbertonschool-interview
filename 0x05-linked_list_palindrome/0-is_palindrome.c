7dcf97ae37c5b65cf828c6a757eea453e6a4d354
rodrigocruz13 <rodrigocruz13>
2020-03-09 14:01:37 -0500

    Removed global variables

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "lists.h"
  
-# define MAX_SIZE 64000
 
 /**
  * is_palindrome- prints all elements of a listint_t list
@@ -13,8 +12,8 @@ int is_palindrome(listint_t **head)
 {
 	const listint_t *current;
 	int i, tam;
-	int array_right[MAX_SIZE];
-	int array_inverse[MAX_SIZE];
+	int array_right[64000];
+	int array_inverse[64000];
 
 	if (head == NULL)
 		return (1);
@@ -28,7 +27,7 @@ int is_palindrome(listint_t **head)
 	for (i = 0, tam = 0; current != NULL; current = current->next, i++, tam++)
 	{
 		array_right[i] = current->n;
-		array_inverse[MAX_SIZE - i - 1] = current->n;
+		array_inverse[64000 - i - 1] = current->n;
 	}
 
 	if (tam < 2)
@@ -37,7 +36,7 @@ int is_palindrome(listint_t **head)
 	}
 	for (i = 0; i < tam; i++)
 	{
-		if (array_right[i] != array_inverse[MAX_SIZE - i - 1])
+		if (array_right[i] != array_inverse[64000 - i - 1])
 		{
 			return (0);
 		}
