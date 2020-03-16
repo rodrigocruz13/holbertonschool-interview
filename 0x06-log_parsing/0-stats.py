77d1c22047c56b960c988142053e8ba819661dfd
rodrigocruz13 <rodrigocruz13>
2020-03-16 10:27:54 -0500

    Task 6

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,49 +3,49 @@
 import sys
 import re
 
-def print_msg(list_status_codes, line_status_codes, total_file_size):
+parsed_line = []
+dict_sc = { 200: "0",
+            301: "0", 
+            400: "0",
+            401: "0",
+            403: "0",
+            404: "0",
+            405: "0",
+            500: "0"
+          }
+total_file_size = 0
+count = 0
+
+def print_msg(dict_sc, total_file_size):
     """
     Method that determines if all the boxes can be opened.
     Args:
-        list_status_codes: list of status codes
-        line_status_codes: # lines per status codes
+        lst_sc: list of status codes
+        count_sc: # lines per status codes
         total_file_size: total of the file
-
     Returns:
         Nothing
     """
 
     print("File size: {}".format(total_file_size))
-    for i in range(len(line_status_codes)):
-        if line_status_codes[i] != 0:
-            print("{}: {}".format(list_status_codes[i], line_status_codes[i]))
+    for i in sorted(dict_sc.keys()):
+        if(dict_sc[i] is not "0"):
+            print("{}: {}".format(i, dict_sc[i]))
 
-
-parsed_line = []
-list_status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-line_status_codes = [0, 0, 0, 0, 0, 0, 0, 0]
-total_file_size = 0
-
-count = 0
 try:
-
     for line in sys.stdin:
-
         parsed_line = re.split(r'[-+#\s*$ ]', line) # ✄ trimming
-        status_code = int(parsed_line[10])  # status code
+        sc = int(parsed_line[10])  # status code
         total_file_size += int(parsed_line[11])  # file size
 
-        if (status_code in list_status_codes):
-            i = list_status_codes.index(status_code)
-            line_status_codes[i] += 1
+        if (sc in sorted(dict_sc.keys())):
+            temp = int(dict_sc[sc]) + 1
+            dict_sc[sc] = str(temp)
 
         if (count % 10 == 0 and count > 0):
-            print_msg(list_status_codes, line_status_codes, total_file_size)
-
+            print_msg(dict_sc, total_file_size)
         count += 1
 
-except KeyboardInterrupt as e:
-    print_msg(list_status_codes, line_status_codes, total_file_size)
-    # print(e)
-
-
+except KeyboardInterrupt:
+    print_msg(dict_sc, total_file_size)
+    sys.exit()
