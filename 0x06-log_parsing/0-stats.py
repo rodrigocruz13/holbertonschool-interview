322e91a61673d112ec92ba7f228a319a300e95ed
rodrigocruz13 <rodrigocruz13>
2020-03-17 17:05:51 -0500

    Task 6

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,25 +3,12 @@
 import sys
 import re
 
-parsed_line = []
-dict_sc = {200: "0",
-           301: "0",
-           400: "0",
-           401: "0",
-           403: "0",
-           404: "0",
-           405: "0",
-           500: "0"}
-total_file_size = 0
-count = 0
-
 
 def print_msg(dict_sc, total_file_size):
     """
-    Method that determines if all the boxes can be opened.
+    Method to print
     Args:
-        lst_sc: list of status codes
-        count_sc: # lines per status codes
+        dict_sc: dict of status codes
         total_file_size: total of the file
     Returns:
         Nothing
@@ -30,11 +17,26 @@ def print_msg(dict_sc, total_file_size):
     print("File size: {}".format(total_file_size))
     for i in sorted(dict_sc.keys()):
         if(dict_sc[i] is not "0"):
-            print("{}: {}".format(i, dict_sc[i]))
+            print("{:}: {:}".format(i, dict_sc[i]))
 
-    try:
-        for line in sys.stdin:
-            parsed_line = re.split(r'[-+#\s*$ ]', line)  # ✄ trimming
+
+parsed_line = []
+dict_sc = {200: "0",
+           301: "0",
+           400: "0",
+           401: "0",
+           403: "0",
+           404: "0",
+           405: "0",
+           500: "0"
+           }
+total_file_size = 0
+count = 0
+
+try:
+    for line in sys.stdin:
+        parsed_line = re.split(r'[-+#\s*$ ]', line)  # ✄ trimming
+        if (len(parsed_line) > 2):
             sc = int(parsed_line[10])  # status code
             total_file_size += int(parsed_line[11])  # file size
 
@@ -42,10 +44,13 @@ def print_msg(dict_sc, total_file_size):
                 temp = int(dict_sc[sc]) + 1
                 dict_sc[sc] = str(temp)
 
-            if (count % 10 == 0 and count > 0):
-                print_msg(dict_sc, total_file_size)
-            count += 1
+        if (count % 10 == 0 and count > 0):
+            print_msg(dict_sc, total_file_size)
+        count += 1
 
-    except KeyboardInterrupt:
-        print_msg(dict_sc, total_file_size)
-        sys.exit()
+except Exception:
+    pass
+
+finally:
+    print_msg(dict_sc, total_file_size)
+sys.exit()
