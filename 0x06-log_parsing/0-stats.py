a69bf1c9bb9c8441eade5fab2863be49efadb66d
rodrigocruz13 <rodrigocruz13>
2020-03-19 03:23:38 -0500

    fixed output

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -16,39 +16,29 @@ def print_msg(dict_sc, total_file_size):
 
     print("File size: {}".format(total_file_size))
     for i in sorted(dict_sc.keys()):
-        if(dict_sc[i] is not "0"):
-            print("{:}: {:d}".format(i, int(dict_sc[i])))
+        if(dict_sc[i] != 0):
+            print("{}: {}".format(i, dict_sc[i]))
 
 
-parsed_line = []
-dict_sc = {200: "0",
-           301: "0",
-           400: "0",
-           401: "0",
-           403: "0",
-           404: "0",
-           405: "0",
-           500: "0"
-           }
 total_file_size = 0
 count = 0
+dict_sc = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 try:
     for line in sys.stdin:
         parsed_line = re.split(r'[-+#\s*$ ]', line)  # ✄ trimming
-        if (len(parsed_line) > 2):
-            sc = int(parsed_line[10])  # status code
-            total_file_size += int(parsed_line[11])  # file size
+
+        if (len(parsed_line) > 9):
+            count += 1
+            if count <= 10:
+                total_file_size += int(parsed_line[11])  # file size
+                sc = int(parsed_line[10])  # status code
 
             if (sc in sorted(dict_sc.keys())):
-                temp = int(dict_sc[sc]) + 1
-                dict_sc[sc] = str(temp)
-            count += 1
+                dict_sc[sc] += 1
 
-        if (count % 10 == 0 and count > 0):
-            print_msg(dict_sc, total_file_size)
+            if (count % 10 == 0):
+                print_msg(dict_sc, total_file_size)
 
-except Exception:
-    pass
 finally:
     print_msg(dict_sc, total_file_size)
