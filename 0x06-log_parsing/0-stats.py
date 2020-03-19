f208b34c8f479f1abe4189212d7f360cce606cb3
rodrigocruz13 <rodrigocruz13>
2020-03-19 04:02:00 -0500

    fixed output

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -23,7 +23,7 @@ def print_msg(dict_sc, total_file_size):
 total_file_size = 0
 count = 0
 dict_sc = {"200": 0,
-           "301": 0, 
+           "301": 0,
            "400": 0,
            "401": 0,
            "403": 0,
@@ -35,17 +35,19 @@ try:
     for line in sys.stdin:
         parsed_line = re.split(r'[-+#\s*$ ]', line)  # ✄ trimming
 
-        if (len(parsed_line) > 9):
+        if len(parsed_line) > 9:
             count += 1
+
             if count <= 10:
                 total_file_size += int(parsed_line[11])  # file size
-                sc = int(parsed_line[10])  # status code
+                sc = parsed_line[10]  # status code
 
-            if (sc in sorted(dict_sc.keys())):
-                dict_sc[sc] += 1
+                if (sc in dict_sc.keys()):
+                    dict_sc[sc] += 1
 
             if (count % 10 == 0):
                 print_msg(dict_sc, total_file_size)
+                count = 0
 
 finally:
     print_msg(dict_sc, total_file_size)
