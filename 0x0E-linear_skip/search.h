cd0fd52e939747b97f018ea7c30431a22015ad17
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-05-27 22:39:29 -0500

    skiptlist

--- 0x0E-linear_skip/search.h
+++ 0x0E-linear_skip/search.h
@@ -24,10 +24,10 @@ typedef struct skiplist_s
     struct skiplist_s *express;
 } skiplist_t;
 
-skiplist_t *create_skiplist(int *array, size_t size);
 void print_skiplist(const skiplist_t *list);
 void free_skiplist(skiplist_t *list);
-skiplist_t *linear_skip(skiplist_t *list, int value);
 
+skiplist_t *linear_skip(skiplist_t *list, int value);
+skiplist_t *create_skiplist(int *array, size_t size);
 
 #endif /* SEARCH_H */
