7625c77bc1140a5aed8d321c0d248d2cf8d6da0f
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-06-08 03:14:53 -0500

    main file

--- 0x10-rain/0-main.py
+++ 0x10-rain/0-main.py
@@ -7,3 +7,24 @@ rain = __import__('0-rain').rain
 if __name__ == "__main__":
     walls = [0, 1, 0, 2, 0, 3, 0, 4]
     print(rain(walls))
+
+    walls = [0, -1, 0, 2, 0, 3, 0, 4]
+    print(rain(walls))
+
+    walls = [0, 'a', 0, 2, 0, 3, 0, 4]
+    print(rain(walls))
+
+    walls = [0]
+    print(rain(walls))
+
+    walls = []
+    print(rain(walls))
+
+    walls = (1, 3, 0, 3, 4)
+    print(rain(walls))
+
+    walls = [1, 3, 0, 3, 4]
+    print(rain(walls))
+
+    walls = [None, None, 0, None, None]
+    print(rain(walls))
