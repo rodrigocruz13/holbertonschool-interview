55eb3d58ac1515046810b6fb3b08bc2fe2a6a465
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-10-01 01:13:18 -0500

    Fixed Variables size incremented

--- 0x15-infinite_multiplication/holberton.h
+++ 0x15-infinite_multiplication/holberton.h
@@ -3,8 +3,8 @@
 
 #define _TRUE_ 1
 #define _FALSE_ 0
-#define MAX1 1000
-#define MAX2 2000
+#define MAX1 2500
+#define MAX2 2500
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -14,6 +14,5 @@ void multiply(char *num_1, char *num_2);
 int is_a_number(char *a_str);
 void sum_and_print(char answer[MAX1][MAX2], int len_1, int len_2);
 int str_len(char *a);
-
 
 #endif /*_MULTI_H_*/
