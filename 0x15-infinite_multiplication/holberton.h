dc776fe51ef310250b7893958d4c799c925a9e3c
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-09-30 20:31:09 -0500

    Removed string.h header include

--- 0x15-infinite_multiplication/holberton.h
+++ 0x15-infinite_multiplication/holberton.h
@@ -6,7 +6,6 @@
 #define MAX1 1000
 #define MAX2 2000
 
-#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -14,5 +13,6 @@ int _putchar(char c);
 void multiply(char *num_1, char *num_2);
 int is_a_number(char *a_str);
 void sum_and_print(char answer[MAX1][MAX2], int len_1, int len_2);
+int str_len(char *a);
 
 #endif /*_MULTI_H_*/
