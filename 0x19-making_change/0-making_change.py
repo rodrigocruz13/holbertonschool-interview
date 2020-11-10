93744cf4129c57023769936202a73681ac75f2bf
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-11-10 12:56:45 -0500

    Fixed comment and variable names

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -18,19 +18,19 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    # verify all coins are integers
+    # verify coins is a valid
     if (coins is None or len(coins) == 0):
         return -1
 
-    c = 0
+    change = 0
     my_coins = sorted(coins, reverse=True)
     money_left = total
 
     for coin in my_coins:
         while (money_left % coin >= 0 and money_left >= coin):
-            c += int(money_left / coin)
+            change += int(money_left / coin)
             money_left = money_left % coin
 
-    c = c if money_left == 0 else -1
+    change = change if money_left == 0 else -1
 
-    return c
+    return change
